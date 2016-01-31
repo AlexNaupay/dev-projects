2da19e2fee9005ae034cc235e34e6e8d7e4870c5
AlexNaupay <alexnaupay@gmail.com>
2016-01-31 17:48:19 -0500

    python - decorators -> movido a dev-learn

--- python/learn-decorators/a4_my_decorator.py
+++
@@ -1,9 +0,0 @@
-def decorator(some_function):
-    def wrapper():
-        print "somethin is happening before some_function is caller"
-
-        some_function()
-
-        print "some_function is called"
-
-    return wrapper
